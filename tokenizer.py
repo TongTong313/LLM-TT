dc4b06550f6121a30514168156cf9373b6253d2c
TongTong <tongtong950313@163.com>
2025-01-31 12:37:22 +0800

    update

---
+++ tokenizer.py
@@ -0,0 +1,21 @@
+# 读取parquet文件
+import pandas as pd
+from tokenizers import Tokenizer
+from tokenizers.models import BPE
+from tokenizers.trainers import BpeTrainer
+from transformers import Qwen2ForCausalLM
+
+tokenizer = Tokenizer(BPE(unk_token="[UNK]"))
+
+trainer = BpeTrainer(
+    special_tokens=["[UNK]", "[CLS]", "[SEP]", "[PAD]", "[MASK]"])
+
+df = pd.read_parquet('./data/train-00000-of-00192.parquet')
+# 判断df中的text是否为空字符串
+empty_text = df['text'].apply(lambda x: x == '')
+# 删除df中text为空字符串的行
+df = df[~empty_text]
+
+print(df.head())
+
+trainer.train(df['text'])
