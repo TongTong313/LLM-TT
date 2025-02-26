3fc7e84378ddde56452c1ce22b66d15480fea49a
TongTong <tongtong950313@163.com>
2025-02-26 13:08:22 +0800

    update

--- tokenizer.py
+++ tokenizer.py
@@ -1,4 +1,3 @@
-# 读取parquet文件
 import pandas as pd
 from tokenizers import Tokenizer
 from tokenizers.models import BPE
@@ -7,12 +6,14 @@ from transformers import Qwen2ForCausalLM
 from typing import Optional
 import torch
 import torch.nn as nn
+from tokenizers.pre_tokenizers import Whitespace, Sequence, Split, ByteLevel
 
 tokenizer = Tokenizer(BPE(unk_token="[UNK]"))
 
 trainer = BpeTrainer(
     special_tokens=["[UNK]", "[CLS]", "[SEP]", "[PAD]", "[MASK]"])
 
+# 读取parquet文件
 df = pd.read_parquet('./data/train-00000-of-00192.parquet')
 # 判断df中的text是否为空字符串
 empty_text = df['text'].apply(lambda x: x == '')
